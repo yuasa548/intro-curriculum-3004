43594cb68c9bacd58a08ef0199885e476513048a
yuasa <y.kyuri2014@gmail.com>
2023-06-29 10:38:30 +0900

    掛け算実装

--- sum/index.js
+++ sum/index.js
@@ -11,4 +11,12 @@ function add(numbers) {
   return result;
 }
 
-module.exports = { add };
+function multi(numbers){
+  let result = 1;
+  for (const num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+module.exports = { add, multi };
